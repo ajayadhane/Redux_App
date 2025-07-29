0a7f84a207fc4215b22d2a3fdd7492e1cfecacaf
gsap_demos <ajadhane1133@gmail.com>
2025-07-29 20:58:34 +0530

    simple counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counter/CounterSlice";
 
 const App = () => {
+  const count = useSelector((state) => state.counter.value);
+
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      dispatch(decrement());
+    }
+  };
+
   return (
     <div className="main-section">
       <h2>First Redux App</h2>
+      <div className="container">
+        <button onClick={handleIncrement}>+</button>
+        <p>{count}</p>
+        <button onClick={handleDecrement}>-</button>
+      </div>
     </div>
   );
 };
